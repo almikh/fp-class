2fd741bea5d1b631f1a7eebfe44d43080581baa0
alexey <alexey.a.mikh@gmail.com>
2014-10-18 17:28:42 +0400

    6е задание сделано полностью

--- class-06/04-db.hs
+++ class-06/04-db.hs
@@ -12,5 +12,80 @@
         ФАМИЛИЯ И.О.
 
 -}
+import System.IO
+import System.Environment
+import System.Directory
 
-main = undefined
+{- несколько вспомогательных, чтоб потом короче писать -}
+parseLine :: String -> [String]
+parseLine s = excGroup ++ [group]
+  where
+    foldFunc (dst, acc) x = if x==';' then (dst ++ [acc], "") else (dst, acc ++ [x])
+    (excGroup, group) = foldl foldFunc ([], "") s
+
+stoi :: String -> Int
+stoi = read
+
+fio2fio :: String -> String
+fio2fio s = f ++ " " ++ ((head i):".") ++ ((head o):".")
+  where [f, i, o] = words s
+
+{- Получение списка из (Курс, [(Группа, Количество_студентов)]) -}
+foldFunc_Count :: [(String, [(String, Int)])] -> [String] -> [(String, [(String, Int)])]
+foldFunc_Count dst [_, _, course, group] = insert dst course group
+  where
+    insert [] insertedCourse insertedGroup = [(insertedCourse, [(insertedGroup, 1)])]
+    insert ((curCourse, groups):otherData) insertedCourse insertedGroup =
+      if curCourse==insertedCourse then (curCourse, insertToGroups groups insertedGroup) : otherData
+      else (curCourse, groups) : (insert otherData insertedCourse insertedGroup)
+    insertToGroups [] insertedGroup = [(insertedGroup, 1)]
+    insertToGroups ((curGroup, students):otherGroups) insertedGroup =
+      if curGroup==insertedGroup then (curGroup, students+1):otherGroups
+      else (curGroup, students) : (insertToGroups otherGroups insertedGroup)
+
+{- Преобразование этого списка в удобочитаемый вид -}
+showFunc_Count :: [(String, [(String, Int)])] -> [String]
+showFunc_Count = foldl showCourse []
+  where
+    showCourse dst (course, groups) = ["course: " ++ course] ++ (map showGroup groups) ++ dst
+    showGroup (group, students) = "  group: " ++ group ++ " -> " ++ (show students) ++ " students"
+
+{- Получение списка элементов вида (<КУРС>_<ГРУППА>, [<студенты>]) -}
+foldFunc_ToFiles :: [(String, [String])] -> [String] -> [(String, [String])]
+foldFunc_ToFiles dst [name, _, course, group] = insert dst (course ++ "_" ++ group, fio2fio name)
+  where
+    insert [] (file, name) = [(file, [name])]
+    insert ((curFile, names):files) (file, name) =
+      if curFile==file then (curFile, name:names) : files
+      else (curFile, names) : (insert files (file, name))
+
+
+{- основные -}
+parse "--mean-age" [course, group, file] = do
+  contents <- readFile file
+  putStrLn $ show $ foldl foldFunc 0 $ map parseLine $ lines contents
+    where
+      foldFunc acc [name, age, cr, gr] = if cr==course && gr==group then acc + (stoi age) else acc
+
+parse "--count" [file] = do
+  contents <- readFile file
+  putStrLn $ unlines $ showFunc_Count $ foldl foldFunc_Count [] $ map parseLine $ lines contents
+
+parse "--to-files" [file] = do
+  contents <- readFile file
+  mapM_ mapFunc $ foldl foldFunc_ToFiles [] $ map parseLine $ lines contents
+    where mapFunc (file, content) = writeFile file $ unlines content
+
+parse _ _ = undefined
+
+help = [
+  "Program options:",
+  "",
+  " --mean-age <course> <group> <file>       1st",
+  " --count <file>                           2nd",
+  " --to-files <file>                        3rd"]
+
+main = do
+  cmd:params <- getArgs
+  if cmd=="-h" then putStrLn $ unlines help
+  else parse cmd params
--- class-06/05-graham.hs
+++ class-06/05-graham.hs
@@ -1,3 +1,52 @@
+{- нужный модуль лежит в другой ветке, поэтому, чтобы не ломать голову, просто вставлю сюда нужный код -}
+import Data.List
+import System.IO
+import System.Environment
+
+data Point = Point { x :: Double, y :: Double }
+  deriving (Show, Eq)
+
+data Direction = RTOL | RTOR | NR
+  deriving (Show, Eq)
+
+fromPair :: (Double, Double) -> Point
+fromPair (x, y) = Point x y
+
+toPair :: Point -> (Double, Double)
+toPair (Point x y) = (x, y)
+
+dist :: Point -> Point -> Double
+dist (Point x1 y1) (Point x2 y2) = sqrt $ (x1-x2)^2 + (y1-y2)^2
+
+dir :: Point -> Point -> Point -> Direction
+dir a b c
+  | ux*vy - uy*vx > 0 = RTOL
+  | ux*vy - uy*vx < 0 = RTOR
+  | otherwise = NR
+    where
+      (ux, uy) = (x b - x a, y b - y a)
+      (vx, vy) = (x c - x a, y c - y a)
+
+graham_scan :: [Point] -> [Point]
+graham_scan = final . sort' . getKeyPoint
+  where
+    getKeyPoint points = foldl func (head points, []) (tail points)
+      where func ((Point x y), others) (Point nx ny) = if nx<x then ((Point nx ny), (Point x y):others) else ((Point x y), (Point nx ny):others)
+    {- сортировка в порядке 'левизны' относительно ключевой точки; (!) если одинаково, то по дистанции до нее -}
+    sort' (key, others) = (key, sortBy sortFunc others)
+      where
+        sortFunc p pp
+          | dir key p pp == RTOL = LT
+          | dir key p pp == NR = if dist key p < dist key pp then LT else GT
+          | otherwise = GT
+    {- удаление лишних точек -}
+    final (key, x:others) = (foldl validationFunc (x:key:[]) others)
+      where
+        removeInvalid (x:y:z:stack)
+          | dir x z y == RTOR = removeInvalid (x:z:stack)
+          | otherwise = (x:y:z:stack)
+        validationFunc acc p = removeInvalid (p:acc)
+
 {-
    Дописать к реализованному ранее алгоритму Грэхема основную программу, которая принимает
    на вход файл со списком точек и создаёт файл со списком точек, образующих выпуклую оболочку.
@@ -8,6 +57,17 @@
      $ ghc 05-graham.hs  -o graham -i../class-05/3-Graham_scan/
 -}
 
-import GrahamScan
+{-
+example: 05-graham inp.txt out.txt
+inp.txt:
+  (1.0, 2.0)
+  (3.0, 4.0)
+  ...
+-}
 
-main = undefined
+
+main = do
+  [inp, out] <- getArgs
+  contents <- readFile inp
+  writeFile out $  unlines $ map show $ map toPair $ graham_scan $ getPoints contents
+    where getPoints c = map fromPair $ map (\x -> read x :: (Double, Double)) $ lines c
